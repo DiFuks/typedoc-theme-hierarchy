c19bb811b2f9d00bb244bdd696f7eb9285bd71b1
Dmitry Fuks <d.fuks@dodobrands.io>
2024-12-19 17:19:23 +0300

    Support typedoc ^0.27.0 and esm

---
+++ packages/plugin/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'tsup';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig([
+	{
+		entry: [`src/index.ts`],
+		format: [`cjs`, `esm`],
+		dts: true,
+		external: [`typedoc-theme-hierarchy`, `typedoc`],
+		banner: ({ format }) => {
+			if (format === `esm`) {
+				return {
+					js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
+				};
+			}
+
+			return {};
+		},
+	},
+]);
